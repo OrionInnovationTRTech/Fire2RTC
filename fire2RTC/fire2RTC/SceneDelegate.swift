5ab4c3b8d7e8aec931574f404eac22e34ea738fb
ece99 <eceserver.ayvaz@gmail.com>
2022-01-19 08:13:41 +0300

    initial commit

---
+++ fire2RTC/fire2RTC/SceneDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  SceneDelegate.swift
+//  fire2RTC
+//
+//  Created by Ece Ayvaz on 19.01.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+      
+    }
+
+
+}
+
