273d6823a2c3eb0f3cc199b710dd4e5566e7d2ff
ece99 <eceserver.ayvaz@gmail.com>
2022-01-25 22:36:31 +0300

    creating firebase connection
    creating firebase connection

---
+++ fire2RTC/fire2RTC/Presentation/WelcomePage/View/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+}
+
